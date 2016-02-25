33a871bee10e6148d32cfe9112c59aeb4399cd2f
Sethmr <seth.rininger@students.fhu.edu>
2016-02-25 11:15:41 -0500

    added Icon and changed file names

--- app/src/main/java/com/example/seth/developmenttest/MainActivity.java
+++ app/src/main/java/com/example/seth/developmenttest/MainActivity.java
@@ -1,187 +1,57 @@
 package com.example.seth.developmenttest;
 
-import android.app.ActionBar;
-import android.app.AlertDialog;
-import android.app.Service;
-import android.bluetooth.BluetoothClass;
+import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.nfc.Tag;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.text.DateFormat;
-import java.util.Date;
+import android.widget.Toast;
 
 
-/**
- * Created by Seth on 2/24/16.
- */
+public class MainActivity extends ActionBarActivity {
 
-public class MainActivity extends Service implements LocationListener {
 
-    private final Context context;
-    boolean isGPSEndabled = false;
-    boolean isNetworkEnabled = false;
-    boolean canGetLocation = false;
+    Button btnShowLocation;
 
-    Location location;
-    double latitude;
-    double longitude;
+    GPSTracker gps;
 
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_test);
 
-    protected LocationManager locationManager;
+        btnShowLocation = (Button) findViewById(R.id.button1);
 
-    public  MainActivity(Context context) {
-        this.context = context;
-        getLocation();
-    }
-
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
-
-            isGPSEndabled = locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
-
-            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if(!isGPSEndabled && !isNetworkEnabled) {
-
-            } else {
-                this.canGetLocation = true;
-
-                if (isNetworkEnabled) {
-                    try { locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this); }
-                    catch (SecurityException e) { e.printStackTrace(); }
-                }
-
-                if (locationManager != null) {
-
-                    try { location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER); }
-                    catch (SecurityException e) { e.printStackTrace(); }
-                    if (location != null) {
-                        latitude = location.getLatitude();
-                        longitude = location.getLongitude();
-                    }
-                }
-            }
-
-            if(isGPSEndabled) {
-                if(location == null) {
-                    try {   locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this); }
-                    catch (SecurityException e) { e.printStackTrace(); }
-
-                    if(locationManager != null) {
-                        try { location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER); }
-                        catch (SecurityException e) { e.printStackTrace(); }
-
-                        if(location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return location;
-    }
-
-    public void stopUsingGPS() {
-        if(locationManager != null) {
-            try { locationManager.removeUpdates(MainActivity.this); }
-            catch (SecurityException e) { e.printStackTrace(); }
-        }
-    }
-
-    public double getLatitude() {
-        if(location != null) {
-            latitude = location.getLatitude();
-        }
-        return latitude;
-    }
-
-    public double getLongitude() {
-        if(location != null) {
-            longitude = location.getLongitude();
-        }
-        return longitude;
-    }
-
-    public boolean canGetLocation() {
-        return this.canGetLocation;
-    }
-
-    public void showSettingsAlert() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-
-        alertDialog.setTitle("GPS is settings");
-
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                context.startActivity(intent);
-            }
-        });
-
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
+            public void onClick(View v) {
+                gps = new GPSTracker(MainActivity.this);
+
+                if (gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    String coords = (String) "Lat: " + latitude + "\nLong: "
+                            + longitude;
+                    setLabel(coords);
+                } else {
+                    gps.showSettingsAlert();
+                }
             }
         });
-
-        alertDialog.show();
     }
 
-
-    @Override
-    public void onLocationChanged(Location l){
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO Auto-generated method stub
-        return null;
+    public void setLabel(String s) {
+        TextView info1 = (TextView) findViewById(R.id.info1);
+        info1.setText(s);
     }
 }
